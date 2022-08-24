b63ebf2bbb272e351d6534c05a11016b31ba5728
HAJINKIM01 <111913297+HAJINKIM01@users.noreply.github.com>
2022-08-23 22:12:21 -0400

    Update and rename main.py to basic.py

---
+++ basic.py
@@ -0,0 +1,11 @@
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+# I learned how to use print function and ++, \n, and debugging. 
+# For now, I will be studying about input function.
+input ("what is your name") # it will allow user to type the answer. 
+print ("hello " + input("what is your name?")+ " it is very nice to meet you")
+
