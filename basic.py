97161d7b1ba8f825255dc082961e36219a16015c
HAJINKIM01 <111913297+HAJINKIM01@users.noreply.github.com>
2022-08-25 23:48:07 -0400

    Update basic.py

--- basic.py
+++ basic.py
@@ -115,5 +115,31 @@ if year%4==0:
     print("leap year")  
 else:
   print("not leap year")
+ # comparing between if/elif/else and multiple if.
+ # when I use if/elif/else에서는 3개중에 조건에 적합한 하나에 들어가는거임. 하지먄 multiple if문에서는 조건에 적합한 모든것들이 들어가는것!
+#띄어쓰기 조심            
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5.")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7.")
+  else:
+    bill = 12
+    print("Adult tickets are $12.")
   
-             
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3
+  
+  print(f"Your final bill is ${bill}")
+
+else:
+  print("Sorry, you have to grow taller before you can ride.")
